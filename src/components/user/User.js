ac44520436aabdc2fdd04e3596ee53718992b1a8
gatwi <faith.murithi@student.moringaschool.com>
2022-07-19 12:49:06 +0300

    the useEffect function

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -12,6 +12,28 @@ function User(props){
         setCount(currentCount + 1)
     }
 
+    function randomize(){
+
+        fetch(RANDOM_USER_API)
+        .then((response) => response.json())
+        .then((jsonData) => {
+            const results = jsonData.results
+            const user = results[0]
+            const userName = `${user.name.title}: ${user.name.first}: ${user.name.last}`
+            const userEmail = `${user.email}`
+            setName(userName)
+            setEmail(userEmail)
+        })
+    }
+// everytime the state is updated, the component re-renders and useEffect will continously run the side effect function.
+
+
+    useEffect(
+        randomize, [count]
+    )
+// introducing the empty dependency array stops the continous loop of fetch results. Therefore, useEffect will run once.
+
+
 
     return(
         <div>
